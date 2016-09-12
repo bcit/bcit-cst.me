bfa2d0e2fa005caf0143c68c16ecbdd5863e97ce
Justin-Leung <lngventure@gmail.com>
2016-09-12 10:00:34 -0700

    Night/Day Theme Timing Switch
    The theme between night/day switches depending on the time of day.

--- js/cleanlinks.js
+++ js/cleanlinks.js
@@ -1,8 +1,34 @@
 $(document).ready(function(){
   $("a").attr('target' ,'_blank');
   
-  var nm = 0;
+	var nm;
+	var d = new Date();
+	var n = d.getHours();
+
+		if(n > 18){
+			nm = 1;
+			$(".nightmode").css("color", "#d9d9d9");
+			$(".nightmode").css("font-style", "italic");
+			$(".list-header").css("color", "#d9d9d9");
+			$(".lead").css("color", "#d9d9d9");
+			$("h1").css("color", "white");
+			$("img").css("filter","invert(90%)");
+			$("img").css("-webkit-filter","invert(90%)");
+			$("body").css("backgroundColor","#1a1a1a");
+		}
+		else{
+			nm = 0;
+			$(".nightmode").css("color", "#686a6e");
+			$(".nightmode").css("font-style", "normal");
+			$(".list-header").css("color", "#686a6e");
+			$(".lead").css("color", "#686a6e");
+			$("h1").css("color", "black");
+			$("img").css("filter","invert(0%)");
+			$("img").css("-webkit-filter","invert(0%)");
+			$("body").css("backgroundColor","white");
+		}
   $(".nightmode").click(function(){
+	
 	    if(nm === 0){
 			nm = 1;
 			$(".nightmode").css("color", "#d9d9d9");
