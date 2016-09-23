00086dbb4696784046488d47012dffe7c5ed6513
Justin-Leung <lngventure@gmail.com>
2016-09-23 14:25:28 -0700

    Link Additions

--- js/cleanlinks.js
+++ js/cleanlinks.js
@@ -4,6 +4,23 @@ $(document).ready(function(){
 	var nm;
 	var d = new Date();
 	var n = d.getHours();
+	var secs= 100; //Change this to change to speed of div aprearing.
+
+	$("body").hide().fadeIn("slow");
+	$("#1").hide().fadeIn("slow");
+	$("#2").hide().delay(2*secs).fadeIn("slow");
+	$("#3").hide().delay(3*secs).fadeIn("slow");
+	$("#4").hide().delay(4*secs).fadeIn("slow");
+	$("#5").hide().delay(5*secs).fadeIn("slow");
+	$("#6").hide().delay(6*secs).fadeIn("slow");
+	$("#7").hide().delay(7*secs).fadeIn("slow");
+	$("#8").hide().delay(8*secs).fadeIn("slow");
+	$("#9").hide().delay(9*secs).fadeIn("slow");
+	$("#10").hide().delay(10*secs).fadeIn("slow");
+	$("#11").hide().delay(11*secs).fadeIn("slow");
+	$("#12").hide().delay(12*secs).fadeIn("slow");
+
+
 
 		if(n > 18 || n < 7){
 			nm = 1;
